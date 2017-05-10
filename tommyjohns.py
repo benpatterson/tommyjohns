844924fe2370f66e0005c79eb4168ff61037b422
Ben Patterson <bpatterson@edx.org>
2017-05-10 07:58:21 -0400

    Remove a lot of commented-out code.

--- tommyjohns.py
+++ tommyjohns.py
@@ -160,48 +160,6 @@ def get_spreadsheet():
     # write out all_stuff as a csv file
 
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     error = None
-#     if request.method == 'POST':
-#         if request.form['username'] != app.config['USERNAME']:
-#             error = 'Invalid username'
-#         elif request.form['password'] != app.config['PASSWORD']:
-#             error = 'Invalid password'
-#         else:
-#             session['logged_in'] = True
-#             flash('You were logged in')
-#             return redirect(url_for('show_entries'))
-#     return render_template('login.html', error=error)
-
-# @app.route('/logout')
-# def logout():
-#     session.pop('logged_in', None)
-#     flash('You were logged out')
-#     return redirect(url_for('show_entries'))
-
-# @app.route('/add', methods=['POST'])
-# def add_entry():
-#     if not session.get('logged_in'):
-#         abort(401)
-#     g.db.execute('insert into spreadsheets (googleuid, spreadsheettitle) values (?, ?)',
-#                  [request.form['googleuid'], request.form['spreadsheettitle']])
-#     # g.db.execute('insert into entries (title, text) values (?, ?)',
-#     #              [request.form['title'], request.form['text']])
-#     g.db.commit()
-#     flash('New entry was successfully posted')
-#     return redirect(url_for('show_entries'))
-
-# @app.route('/add', methods=['POST'])
-# def add_entry():
-#     if not session.get('logged_in'):
-#         abort(401)
-#     g.db.execute('insert into spreadsheets (googleuid, spreadsheettitle) values (?, ?)',
-#                  [request.form['googleuid'], request.form['spreadsheet title']])
-#     g.db.commit()
-#     flash('New entry was successfully posted')
-#     return redirect(url_for('show_entries'))
-
 if __name__ == '__main__':
     init_db()
     build_charts()
